f2738247ba1402942337a7e79e9a4728a9050de9
vishal24149732 <vishal.manjusrinivasan@mail.bcu.ac.uk>
2026-02-01 19:49:35 +0000

    lab1

--- Labs/week1/Task2.cpp
+++ Labs/week1/Task2.cpp
@@ -31,10 +31,31 @@ int main()
 	//           should be dark in your output. 
 	//           Hint: if the pixels ranged in value from 0 to 1, you could replace each pixel value (v) with (1 - v). 
 	//           but remember, the pixels have 8-bit unsigned values, so range from 0 to 255!
+	// 
+	
+	for (int y = 0; y < height; ++y)
+		for (int x = 0; x < width; ++x)
+			for (int c = 0; c < 3; ++c) {
+				int pixelIdx = x + y * width;
+				imageBuffer[pixelIdx * 4 + c] = 255 - imageBuffer[pixelIdx * 4 + c];
+			}
+
 	// * Optional Task 2: Try downsampling this image to 1/2 resolution.
 	//           You can either just keep one in every 4 pixels, or better yet, average the pixels in each 2x2 block.
 	//           Hint: Be careful when averaging! You probably want to convert the pixel values to floating-point to
 	//           do the averaging maths.
+
+	for (int y = 0; y < (height/2); ++y)
+		for (int x = 0; x < (width/2); ++x)
+			for (int c = 0; c < 3; ++c) {
+				int pixelIdx1 = x + y * width;
+				int pixelIdx2 = (x + 1) + (y) * width;
+				int pixelIdx3 = (x)+(y + 1) * width;
+				int pixelIdx4 = (x + 1) + (y + 1) * width;
+				float avg = (imageBuffer[pixelIdx1 * 4 + c] + imageBuffer[pixelIdx2 * 4 + c] +
+					imageBuffer[pixelIdx3 * 4 + c] + imageBuffer[pixelIdx4 * 4 + c]) / 4.0f;
+				imageBuffer[pixelIdx1 * 4 + c] = static_cast<uint8_t>(avg);
+			}
 
 	int errorCode;
 	errorCode = lodepng::encode(outputFilename, imageBuffer, width, height);
