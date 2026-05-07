90e455d20db26ef439a0425e7dd56a19e27cfd8c
vishal24149732 <vishal.manjusrinivasan@mail.bcu.ac.uk>
2026-05-07 12:41:13 +0100

    final tweaks of ratitriser and raytracer

--- Coursework/Rasteriser/rasteriser.cpp
+++ Coursework/Rasteriser/rasteriser.cpp
@@ -1,208 +1,3 @@
-//// This define is necessary to get the M_PI constant.
-//#define _USE_MATH_DEFINES
-//#include <math.h>
-//
-//#include <iostream>
-//#include <lodepng.h>
-//#include "Mesh.hpp"
-//
-//
-//Eigen::Vector4f vec3ToVec4(const Eigen::Vector3f& v)
-//{
-//	Eigen::Vector4f output;
-//	output << v.x(), v.y(), v.z(), 1.0f;
-//	return output;
-//}
-//
-//
-//float vec2Cross(const Eigen::Vector2f& v0, const Eigen::Vector2f& v1)
-//{
-//	return v0.x() * v1.y() - v0.y() * v1.x();
-//}
-//
-//void setPixel(std::vector<uint8_t>& image, int x, int y, int width, int height, uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255)
-//{
-//	int pixelIdx = x + y * width;
-//	image[pixelIdx * 4 + 0] = r;
-//	image[pixelIdx * 4 + 1] = g;
-//	image[pixelIdx * 4 + 2] = b;
-//	image[pixelIdx * 4 + 3] = a;
-//}
-//
-//void drawTriangle(std::vector<uint8_t>& image, int width, int height,
-//	const Eigen::Vector2f& p0, const Eigen::Vector2f& p1, const Eigen::Vector2f& p2,
-//	uint8_t r, uint8_t g, uint8_t b, uint8_t a = 255)
-//{
-//	
-//	int minX, minY, maxX, maxY;
-//	minX = std::min(std::min(p0.x(), p1.x()), p2.x());
-//	minY = std::min(std::min(p0.y(), p1.y()), p2.y());
-//	maxX = std::max(std::max(p0.x(), p1.x()), p2.x());
-//	maxY = std::max(std::max(p0.y(), p1.y()), p2.y());
-//
-//	
-//	minX = std::min(std::max(minX, 0), width);
-//	maxX = std::min(std::max(maxX, 0), width);
-//	minY = std::min(std::max(minY, 0), height);
-//	maxY = std::min(std::max(maxY, 0), height);
-//
-//	Eigen::Vector2f edge1 = p1 - p0;
-//	Eigen::Vector2f edge2 = p2 - p0;
-//	float triangleArea = 0.5f * vec2Cross(edge2, edge1);
-//	if (triangleArea < 0) {
-//		
-//		return;
-//	}
-//
-//	for (int x = minX; x <= maxX; ++x)
-//		for (int y = minY; y <= maxY; ++y) {
-//			Eigen::Vector2f p(x, y);
-//
-//			float a0 = 0.5f * fabsf(vec2Cross(p1 - p2, p - p2));
-//			float a1 = 0.5f * fabsf(vec2Cross(p0 - p2, p - p2));
-//			float a2 = 0.5f * fabsf(vec2Cross(p0 - p1, p - p1));
-//
-//			float b0 = a0 / triangleArea;
-//			float b1 = a1 / triangleArea;
-//			float b2 = a2 / triangleArea;
-//
-//			float sum = b0 + b1 + b2;
-//			if (sum > 1.0001) {
-//				continue;
-//			}
-//
-//			setPixel(image, x, y, width, height, r, g, b, a);
-//		}
-//}
-//
-//
-//void drawMesh(std::vector<unsigned char>& image, const Mesh& mesh,
-//	const Eigen::Vector3f& baseColor, const Eigen::Matrix4f& transform,
-//	int width, int height)
-//{
-//	for (const auto& face : mesh.faces) {
-//		Eigen::Vector3f
-//			v0 = mesh.verts[face[0]],
-//			v1 = mesh.verts[face[1]],
-//			v2 = mesh.verts[face[2]];
-//
-//		Eigen::Vector4f tv0, tv1, tv2;
-//
-//		
-//		Eigen::Vector4f v0h = vec3ToVec4(v0);
-//		Eigen::Vector4f v1h = vec3ToVec4(v1);
-//		Eigen::Vector4f v2h = vec3ToVec4(v2);
-//		tv0 = transform * v0h;
-//		tv1 = transform * v1h;
-//		tv2 = transform * v2h;
-//		
-//
-//		Eigen::Vector2f p0(tv0.x() * 250 + width / 2, -tv0.y() * 250 + height / 2);
-//		Eigen::Vector2f p1(tv1.x() * 250 + width / 2, -tv1.y() * 250 + height / 2);
-//		Eigen::Vector2f p2(tv2.x() * 250 + width / 2, -tv2.y() * 250 + height / 2);
-//
-//
-//		Eigen::Vector3f edge1 = tv1.block<3, 1>(0, 0) - tv0.block<3, 1>(0, 0);
-//		Eigen::Vector3f edge2 = tv2.block<3, 1>(0, 0) - tv0.block<3, 1>(0, 0);
-//		Eigen::Vector3f normal = edge1.cross(edge2).normalized();
-//
-//		float intensity = normal.dot(Eigen::Vector3f(0, 0, 1));
-//		if (intensity > 0.f) {
-//			drawTriangle(image, width, height, p0, p1, p2,
-//				baseColor.x() * intensity * 255,
-//				baseColor.y() * intensity * 255,
-//				baseColor.z() * intensity * 255);
-//		}
-//	}
-//}
-//
-//
-//Eigen::Matrix4f translationMatrix(const Eigen::Vector3f& t)
-//{
-//	
-//	Eigen::Matrix4f M = Eigen::Matrix4f::Identity();
-//	M(0, 3) = t.x();
-//	M(1, 3) = t.y();
-//	M(2, 3) = t.z();
-//	return M;
-//}
-//
-//
-//Eigen::Matrix4f scaleMatrix(float s)
-//{
-//	// *** Your code here ***
-//	Eigen::Matrix4f M = Eigen::Matrix4f::Identity();
-//	M(0, 0) = s;
-//	M(1, 1) = s;
-//	M(2, 2) = s;
-//	return M;
-//}
-//
-//
-//Eigen::Matrix4f rotateYMatrix(float theta)
-//{
-//	
-//	Eigen::Matrix4f M = Eigen::Matrix4f::Identity();
-//	M(0, 0) = cosf(theta);
-//	M(0, 2) = sinf(theta);
-//	M(2, 0) = -sinf(theta);
-//	M(2, 2) = cosf(theta);
-//	return M;
-//}
-//
-//int main()
-//{
-//
-//	std::string outputFilename = "output.png";
-//
-//	const int width = 512, height = 512;
-//	const int nChannels = 4;
-//
-//	std::vector<uint8_t> imageBuffer(height * width * nChannels);
-//
-//	
-//	memset(&imageBuffer[0], 0, width * height * nChannels * sizeof(uint8_t));
-//
-//	std::string bunnyFilename = "../models/stanford_bunny_simplified.obj";
-//	std::string dragonFilename = "../models/stanford_dragon_simplified.obj";
-//	std::string spiderManFilename = "../models/sm.obj";
-//
-//	Mesh bunnyMesh = loadMeshFile(bunnyFilename);
-//	Mesh dragonMesh = loadMeshFile(dragonFilename);
-//	Mesh spiderManMesh = loadMeshFile(spiderManFilename);
-//
-//
-//	
-//
-//	Eigen::Matrix4f bunnyTransform = Eigen::Matrix4f::Identity();
-//	Eigen::Matrix4f dragonTransform = Eigen::Matrix4f::Identity();
-//	Eigen::Matrix4f spiderManTransform = Eigen::Matrix4f::Identity();
-//	std::cout << "huh: " << bunnyTransform << std::endl;
-//	bunnyTransform =
-//		translationMatrix(Eigen::Vector3f(-0.5f, -0.2f, 0.0f)) * rotateYMatrix(0.5f) * scaleMatrix(0.01f);
-//	std::cout << "myMat4: " << bunnyTransform << std::endl;
-//	dragonTransform =
-//		translationMatrix(Eigen::Vector3f(0.5f, 0.2f, 0.0f)) * rotateYMatrix(1.0f) * scaleMatrix(0.01f);
-//	spiderManTransform =
-//		translationMatrix(Eigen::Vector3f(0.0f, 0.0f, 0.0f)) * rotateYMatrix(0.5f) * scaleMatrix(0.5f);
-//
-//	
-//
-//	drawMesh(imageBuffer, bunnyMesh, Eigen::Vector3f(0, 1, 0), bunnyTransform, width, height);
-//	drawMesh(imageBuffer, dragonMesh, Eigen::Vector3f(0, 1, 1), dragonTransform, width, height);
-//	drawMesh(imageBuffer, spiderManMesh, Eigen::Vector3f(0, 1, 1), spiderManTransform, width, height);
-//
-//	
-//	int errorCode;
-//	errorCode = lodepng::encode(outputFilename, imageBuffer, width, height);
-//	if (errorCode) { 
-//		std::cout << "lodepng error encoding image: " << lodepng_error_text(errorCode) << std::endl;
-//		return errorCode;
-//	}
-//
-//	return 0;
-//}
-
 
 #define _USE_MATH_DEFINES
 #include <math.h>
@@ -301,7 +96,7 @@ void drawTriangle(
 
             
 
-            // -------- Perspective Correct --------
+            
             float d0 = t.cam[0].z();
             float d1 = t.cam[1].z();
             float d2 = t.cam[2].z();
@@ -319,28 +114,17 @@ void drawTriangle(
 
             //std::cout << depth << std::endl;
 
-            Eigen::Vector3f worldP =
-                depth * ((t.verts[0] * b0) / d0 +
-                    (t.verts[1] * b1) / d1 +
-                    (t.verts[2] * b2) / d2);
+            Eigen::Vector3f worldP = depth * ((t.verts[0] * b0) / d0 + (t.verts[1] * b1) / d1 + (t.verts[2] * b2) / d2);
 
-            Eigen::Vector3f normP =
-                ((t.norms[0] * b0) / d0 +
-                    (t.norms[1] * b1) / d1 +
-                    (t.norms[2] * b2) / d2).normalized();
+            Eigen::Vector3f normP = ((t.norms[0] * b0) / d0 + (t.norms[1] * b1) / d1 + (t.norms[2] * b2) / d2).normalized();
 
-            // -------- TEXTURE HOOK --------
-            // Eigen::Vector2f uv = ...
-            // Color texColor = sampleTexture(...)
-            // Eigen::Vector3f finalAlbedo = texColor * albedo;
             float w0 = 1.0f / d0;
             float w1 = 1.0f / d1;
             float w2 = 1.0f / d2;
 
             float denom = (b0 * w0 + b1 * w1 + b2 * w2);
 
-            Eigen::Vector2f uv =
-                (t.texs[0] * b0 * w0 +
+            Eigen::Vector2f uv =  (t.texs[0] * b0 * w0 +
                     t.texs[1] * b1 * w1 +
                     t.texs[2] * b2 * w2) / denom;
 
@@ -617,7 +401,7 @@ int main()
     Eigen::Vector3f camPos =
         (cameraToWorld * Eigen::Vector4f(0, 0, 0, 1)).head<3>();
 
-    // -------- LIGHTS --------
+    // lights ... 
     std::vector<std::unique_ptr<Light>> lights;
 
     lights.emplace_back(new AmbientLight(Eigen::Vector3f(0.12f, 0.09f, 0.08f)));
@@ -636,7 +420,7 @@ int main()
     Eigen::Vector3f fogColor(0.85f, 0.65f, 0.5f);
     
 
-    // -------- LOAD MODEL --------
+    // loading the models
     
     Mesh spiderman = loadMeshFile("../models/sm_final6.obj");
 
@@ -663,7 +447,7 @@ int main()
    
 
     drawMesh(image, zBuffer, spiderman,
-        Eigen::Vector3f(1.0f, 1.0f, 1.0f),   // base color (replace with texture later)
+        Eigen::Vector3f(1.0f, 1.0f, 1.0f), 
         Eigen::Vector3f(1.2f, 1.2f, 1.2f),
         300.0f,
         ShadingMode::BLINN_PHONG,
@@ -677,7 +461,7 @@ int main()
         width,
         height, fogColor);
 
-    //------
+    // building models
     std::vector<uint8_t> textureImage2;
     unsigned texWidth2, texHeight2;
 
@@ -696,7 +480,7 @@ int main()
 
 
     drawMesh(image, zBuffer, build2,
-        Eigen::Vector3f(1.0f, 1.0f, 1.0f),   // base color (replace with texture later)
+        Eigen::Vector3f(1.0f, 1.0f, 1.0f),   
         Eigen::Vector3f(1.2f, 1.2f, 1.2f),
         300.0f,
         ShadingMode::BLINN_PHONG,
@@ -729,7 +513,7 @@ int main()
 
 
     drawMesh(image, zBuffer, build3,
-        Eigen::Vector3f(1.0f, 1.0f, 1.0f),   // base color (replace with texture later)
+        Eigen::Vector3f(1.0f, 1.0f, 1.0f),   
         Eigen::Vector3f(1.2f, 1.2f, 1.2f),
         300.0f,
         ShadingMode::BLINN_PHONG,
@@ -762,7 +546,7 @@ int main()
 
 
     drawMesh(image, zBuffer, build4,
-        Eigen::Vector3f(1.0f, 1.0f, 1.0f),   // base color (replace with texture later)
+        Eigen::Vector3f(1.0f, 1.0f, 1.0f),   
         Eigen::Vector3f(1.2f, 1.2f, 1.2f),
         300.0f,
         ShadingMode::BLINN_PHONG,
@@ -795,7 +579,7 @@ int main()
 
 
     drawMesh(image, zBuffer2, build5,
-        Eigen::Vector3f(1.0f, 1.0f, 1.0f),   // base color (replace with texture later)
+        Eigen::Vector3f(1.0f, 1.0f, 1.0f),   
         Eigen::Vector3f(1.2f, 1.2f, 1.2f),
         300.0f,
         ShadingMode::BLINN_PHONG,
@@ -854,8 +638,6 @@ int main()
     }
 
     
-
-    // -------- SAVE --------
     lodepng::encode("output.png", finalImage, finalWidth, finalHeight);
 
     return 0;
--- Coursework/Raytracer/main.cpp
+++ Coursework/Raytracer/main.cpp
@@ -64,15 +64,40 @@ int main(int argc, char* argv[]) {
 		lavender(178.f / 255.f, 164.f / 255.f, 212.f / 255.f);
 
 	// *** Load shaders and textures ***
+	std::vector<uint8_t> skyTex;
+	unsigned int width6, height6;
+	lodepng::decode(skyTex, width6, height6, "../models/sky.png");
+
+
 	std::vector<uint8_t> spiderTexture;
 	unsigned int width, height;
 	lodepng::decode(spiderTexture, width, height, "../models/tex.png");
+
+	std::vector<uint8_t> buildTex1;
+	unsigned int width2, height2;
+	lodepng::decode(buildTex1, width2, height2, "../models/build1Tex.png");
+
+	std::vector<uint8_t> buildTex2;
+	unsigned int width3, height3;
+	lodepng::decode(buildTex2, width3, height3, "../models/build2Tex.png");
+
+	std::vector<uint8_t> buildTex3;
+	unsigned int width4, height4;
+	lodepng::decode(buildTex3, width4, height4, "../models/build3Tex.png");
+
+	std::vector<uint8_t> buildTex4;
+	unsigned int width5, height5;
+	lodepng::decode(buildTex4, width5, height5, "../models/build4Tex.png");
 
 	LambertianShader redLambertianShader(red);
 	PhongShader bluePlasticShader(blue, Eigen::Vector3f(1.f, 1.f, 1.f), 100.f);
 	LambertianShader aquaLambertianShader(aqua);
 	LambertianShader lavenderLambertianShader(lavender);
 	TexturedLambertianShader spiderShader(&spiderTexture, width, height);
+	TexturedLambertianShader build1Shader(&buildTex1, width2, height2);
+	TexturedLambertianShader build2Shader(&buildTex2, width3, height3);
+	TexturedLambertianShader build3Shader(&buildTex3, width4, height4);
+	TexturedLambertianShader build4Shader(&buildTex4, width5, height5);
 	MirrorShader mirrorShader;
 	TexCoordTestShader texCoordTestShader;
 
@@ -81,8 +106,37 @@ int main(int argc, char* argv[]) {
 
 	// Optional code: here's how to add the spider mesh to the scene, using a BVH
 	// Try enabling this and comparing it to the non-BVH version below!
+	Eigen::Matrix4f transform =
+		makeTranslationMatrix(Eigen::Vector3f(-8.0f, -8.0f, -15.0f)) *
+		rotateX(0.1f) * uniformScale(5.0f);
 	Model spiderMan("../models/sm_final6.obj");
-	scene.renderables.push_back(std::make_shared<BVHNode>(spiderMan, &spiderShader, 4,  rotateY(M_PI / 4.0f)));
+	scene.renderables.push_back(std::make_shared<BVHNode>(spiderMan, &spiderShader, 4,  transform));
+
+	//-----
+	Eigen::Matrix4f transform2 =
+		makeTranslationMatrix(Eigen::Vector3f(85.0f, -55.0f, -8.0f)) *
+		rotateX(0.1f) * uniformScale(50.0f);
+	Model build1("../models/build1.obj");
+	scene.renderables.push_back(std::make_shared<BVHNode>(build1, &build1Shader, 4, transform2));
+
+	Eigen::Matrix4f transform3 =
+		makeTranslationMatrix(Eigen::Vector3f(-3.0f, -10.0f, -8.0f)) *
+		rotateX(0.1f) * uniformScale(5.0f);
+	Model build2("../models/build2.obj");
+	scene.renderables.push_back(std::make_shared<BVHNode>(build2, &build2Shader, 4, transform3));
+
+	Eigen::Matrix4f transform4 =
+		makeTranslationMatrix(Eigen::Vector3f(0.0f, -10.0f, -8.0f)) *
+		rotateX(0.1f) * uniformScale(5.0f);
+	Model build3("../models/build3.obj");
+	scene.renderables.push_back(std::make_shared<BVHNode>(build3, &build3Shader, 4, transform4));
+
+	Eigen::Matrix4f transform5 =
+		makeTranslationMatrix(Eigen::Vector3f(5.0f, -10.0f, -8.0f)) *
+		rotateX(0.1f) * uniformScale(5.0f);
+	Model build4("../models/build4.obj");
+	scene.renderables.push_back(std::make_shared<BVHNode>(build4, &build4Shader, 4, transform5));
+
 
 	// Here's how to add the mesh without using the BVH.
 	// Try comparing performance to the BVH version above.
@@ -117,34 +171,116 @@ int main(int argc, char* argv[]) {
 	scene.intersect(ray, 1e-6f, 1e6f, hitInfo, VISIBLE_BITMASK);
 	float x = hitInfo.hitT;
 
+	const int samplesPerPixel = 4;
+
+	Eigen::Vector3f fogColor(0.7f, 0.7f, 0.75f);
+	/*float fogStart = 25.0f;
+	float fogEnd = 80.0f;*/
+	float fogDensity = 0.03f;
+	float fogHeightFalloff = 0.15f;
+	float fogStrength = 0.4f;
+
+	float aperture = 0.1f;
+	float focusDistance = 15.0f;
+	Eigen::Vector3f camForward = loadVec3FromConfig(config["cameraForward"]).normalized();
+	Eigen::Vector3f camRight =camForward.cross(loadVec3FromConfig(config["cameraUp"])).normalized();
+	Eigen::Vector3f camUp =camRight.cross(camForward).normalized();
 
 	#pragma omp parallel for
 	for (int y = 0; y < pixHeight; ++y) {
 		for (int x = 0; x < pixWidth; ++x) {
-			Ray ray = cam.getRay(x, scanlines[y]);
-			HitInfo hitInfo;
-			if (scene.intersect(ray, 1e-6f, 1e6f, hitInfo, VISIBLE_BITMASK)) {
-				Eigen::Vector3f color = hitInfo.shader->getColor(
-					hitInfo, &scene,
-					lightSources, ambientLight,
-					0, config["maxBounces"]);
+			Eigen::Vector3f finalColor(0.f, 0.f, 0.f);
+			bool hit = false;
+			for (int s = 0; s < samplesPerPixel; ++s)
+			{
+				//takes random offset inside each pixel
+				float offsetX = (float)rand() / RAND_MAX;
+				float offsetY = (float)rand() / RAND_MAX;
 
-				color.x() = std::min(color.x(), 1.f);
-				color.y() = std::min(color.y(), 1.f);
-				color.z() = std::min(color.z(), 1.f);
+				//depth of field. creates a lens, adds a new orgin to the lens and changes the ray direction to the focus point from that lens.
+				Ray centerRay = cam.getRay(x + offsetX,scanlines[y] + offsetY);
+				Eigen::Vector3f focusPoint =centerRay.origin +centerRay.direction * focusDistance;
 
+				float lensX = (((float)rand() / RAND_MAX) - 0.5f) * aperture;
+
+				float lensY = (((float)rand() / RAND_MAX) - 0.5f) * aperture;
+
+				Eigen::Vector3f newOrigin =centerRay.origin +camRight * lensX +camUp * lensY;
+
+				Eigen::Vector3f newDirection =(focusPoint - newOrigin).normalized();
+
+				Ray ray;
+				ray.origin = newOrigin;
+				ray.direction = newDirection;
+
+
+				HitInfo hitInfo;
+
+				if (scene.intersect(ray, 1e-6f, 1e6f, hitInfo, VISIBLE_BITMASK))
+				{
+					Eigen::Vector3f color =hitInfo.shader->getColor(hitInfo,&scene,lightSources,ambientLight,0,config["maxBounces"]);
+
+					if (hitInfo.shader != &spiderShader)
+					{
+						float distance = hitInfo.hitT;
+						Eigen::Vector3f worldPos = ray.origin + ray.direction * distance; 
+
+						float heightFog = exp(-worldPos.y() * fogHeightFalloff);
+						
+						float distanceFog = 1.0f - exp(-distance * fogDensity);
+
+						//float fogFactor = (distance - fogStart) / (fogEnd - fogStart);
+
+						float fogFactor = distanceFog * heightFog;
+
+						fogFactor = std::min(std::max(fogFactor, 0.f), 1.f);
+
+						fogFactor *= fogStrength;
+
+						color = (1.0f - fogFactor) * color + fogFactor * fogColor;
+					}
+
+					finalColor += color;
+					hit = true;
+				}
+			}
+			if(hit) 
+			{
+				finalColor /= float(samplesPerPixel);
+
+				finalColor.x() = std::min(finalColor.x(), 1.f);
+				finalColor.y() = std::min(finalColor.y(), 1.f);
+				finalColor.z() = std::min(finalColor.z(), 1.f);
 
 				int line = (pixHeight - scanlines[y]) - 1;
-				outImage[(x + line * pixWidth) * nChannels + 0] = color.x() * 255;
-				outImage[(x + line * pixWidth) * nChannels + 1] = color.y() * 255;
-				outImage[(x + line * pixWidth) * nChannels + 2] = color.z() * 255;
+
+				outImage[(x + line * pixWidth) * nChannels + 0] =finalColor.x() * 255;
+
+				outImage[(x + line * pixWidth) * nChannels + 1] =finalColor.y() * 255;
+
+				outImage[(x + line * pixWidth) * nChannels + 2] =finalColor.z() * 255;
+
 				outImage[(x + line * pixWidth) * nChannels + 3] = 255;
 			}
-			else {
+			else 
+			{
 				int line = (pixHeight - scanlines[y]) - 1;
-				outImage[(x + line * pixWidth) * nChannels + 0] = 0;
-				outImage[(x + line * pixWidth) * nChannels + 1] = 0;
-				outImage[(x + line * pixWidth) * nChannels + 2] = 0;
+
+				
+				float u = float(x) / float(pixWidth);
+				float v = float(line) / float(pixHeight);
+
+				int texX = std::min(int(u * width6), int(width6 - 1));
+				int texY = std::min(int(v * height6), int(height6 - 1));
+
+				int bgIndex = (texX + texY * width6) * 4;
+
+				outImage[(x + line * pixWidth) * nChannels + 0] =skyTex[bgIndex + 0];
+
+				outImage[(x + line * pixWidth) * nChannels + 1] =skyTex[bgIndex + 1];
+
+				outImage[(x + line * pixWidth) * nChannels + 2] =skyTex[bgIndex + 2];
+
 				outImage[(x + line * pixWidth) * nChannels + 3] = 255;
 			}
 		}
